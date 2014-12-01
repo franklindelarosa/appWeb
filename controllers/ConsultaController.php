195208660db623de5d8667c937a059df07b715a3
Oswaldo De la Ossa <oswaldo244@gmail.com>
2014-12-01 18:02:13 -0500

    usuariosModal done

--- controllers/ConsultaController.php
+++ controllers/ConsultaController.php
@@ -20,4 +20,25 @@ class ConsultaController extends Controller
         ]);
     }
 
+    public function actionEquipos(){
+    	$sql = "CALL jugadoresEquipo('b',".$_POST['id'].")";
+        $equipos[0] = \Yii::$app->db->createCommand($sql)->queryAll();
+        $sql = "CALL jugadoresEquipo('n',".$_POST['id'].")";
+        $equipos[1] = \Yii::$app->db->createCommand($sql)->queryAll();
+        $equipos[2] = max(count($equipos[0]),count($equipos[1]));
+        \Yii::$app->response->format = 'json';
+
+        return $equipos;
+    }
+
+    public function actionUsuario(){
+        $query = (new \yii\db\Query());
+        $query->select('nombre,usuario,sexo,')->from('Usuarios')->where('id_usuario =:id');
+        $query->addParams(['id'=>$_POST['id']]);
+        $user = $query->one();
+        \Yii::$app->response->format = 'json';
+
+        return $user;
+    }
+
 }
--- views/consulta/index.php
+++ views/consulta/index.php
@@ -1,10 +1,39 @@
 <script type="text/javascript">
     $(document).ready(function() {
-        $('tr').on('dblclick', function(event) {
+        $('#TablaConsulta tr').on('dblclick', function(event) {
             event.preventDefault();
+            var data = $(this).attr('data-key');
+            $.post('equipos', {id: data}).done(function(data) {
+                generarTabla(data[0],'equipoBlanco','Blanco',data[2]-data[0].length);
+                generarTabla(data[1],'equipoNegro','Negro',data[2]-data[1].length);
+                // console.log(data);
+            });
             $('#myModal').modal({backdrop:'static'});
         });
+
+        $('#cuerpoModal').on('dblclick','td', function(event) {
+            event.preventDefault();
+            var data = $(this).attr('data-id');
+            if(data!=null){
+                $.post('usuario', {id: data}).done(function(data) {
+                    $('#usuarioModal').modal({backdrop:'static'});
+                    console.log(data);
+                });
+            }else{alert('Seleccione un usuario válido')}
+        });
+
     });
+
+    function generarTabla(data,tabla,equipo,n){        
+        $('#'+tabla).empty();
+        $('#'+tabla).append('<tr><th class="text-center"> Equipo '+equipo+'</th></tr>');
+        $.each(data, function(index, val) {
+             $('#'+tabla).append('<tr><td data-id='+val['id_usuario']+' class="text-center">'+val['nombre']+'</td></tr>');
+        });
+        for (var i = 0; i < n; i++) {
+            $('#'+tabla).append('<tr><td class="text-center">&nbsp</td></tr>');
+        };
+    }
 </script>
 
 <?php
@@ -57,12 +86,30 @@ $this->params['breadcrumbs'][] = $this->title;
                 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                 <h4 class="modal-title">Equipos</h4>
             </div>
-            <div class="modal-body">
-                <table id="tableDetalle" class="table table-responsive table-striped table-hover">
-                    <tr>
-                        <th>Equipo Blanco</th>
-                        <th>Equipo Negro</th>
-                    </tr>
+            <div id="cuerpoModal" class="modal-body">
+                <table id="equipoBlanco" class="tablaModal table table-striped table-bordered table-hover">
+                </table>
+                 <table id="equipoNegro" class="tablaModal table table-striped table-bordered table-hover">
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="usuarioModal" class="modal fade bs-example-modal-sm" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Usuario</h4>
+            </div>
+            <div id="cuerpoModal" class="modal-body">
+                <table id="equipoBlanco" class="tablaModal table table-striped table-bordered table-hover">
+                </table>
+                 <table id="equipoNegro" class="tablaModal table table-striped table-bordered table-hover">
                 </table>
             </div>
             <div class="modal-footer">

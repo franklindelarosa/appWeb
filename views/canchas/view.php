7299f72653e8ee9db20248a4dc9474da7ed276e3
Oswaldo De la Ossa <oswaldo244@gmail.com>
2014-12-03 10:32:17 -0500

    colocando imagenes

--- views/canchas/view.php
+++ views/canchas/view.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="canchas-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1><p><img src="http://localhost/futbolcracks/web/images/futbolCracks.png" data-toggle="modal" data-target="#LogoModal" alt="logo" style="width:128px;height:128px"><p>
 
     <p class="btn-right"><a href="<?= Yii::$app->request->baseUrl; ?>/canchas/index" class="btn btn-default">Volver</a></p>
     <p>
@@ -24,6 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <a href="#" data-toggle="modal" data-target="#canchaModal" class="btn btn-warning">Foto de la cancha</a>
     </p>
 
     <?= DetailView::widget([
@@ -37,4 +38,44 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+</div>
+
+<div id="LogoModal" class="modal fade bs-example-modal-sm" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Logo</h4>
+            </div>
+            <div class="modal-body text-center">
+                <div class="row">
+                    <p><img src="http://localhost/futbolcracks/web/images/futbolCracks.png" alt="logo" style="width:128px;height:128px"><p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Cambiar imagen</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="canchaModal" class="modal fade bs-example-modal-sm" data-backdrop="false" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Cancha</h4>
+            </div>
+            <div class="modal-body text-center">
+                <div class="row">
+                    <p><img src="http://localhost/futbolcracks/web/images/field.png" alt="cancha" style="width:128px;height:128px"><p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Cambiar imagen</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+    </div>
 </div>
